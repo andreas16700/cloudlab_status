5a225a851cf6a26c480cb80343f6676053188a2a
Andreas Loizides <andreas16700@icloud.com>
2022-10-20 19:21:29 +0300

    Initial Commit

---
+++ CL_Status/CL_StatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  CL_StatusApp.swift
+//  CL_Status
+//
+//  Created by Andreas Loizides on 20/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct CL_StatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CL_Status/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CL_Status
+//
+//  Created by Andreas Loizides on 20/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
